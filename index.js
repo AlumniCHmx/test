c2531dad8216ac48e002d089b5a02e6db45b22e9
JesusMoralesF <emorales41@gmail.com>
2020-07-02 22:39:43 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,56 @@
+var altura=window.innerHeight;
+var maxAlt=altura-25;
+
+var cajas=document.querySelectorAll('.blurred-box');
+cajas.forEach(function(element) {
+    element.setAttribute('onclick','print(this)')
+});
+
+/* //UAM
+var areasArray=[
+    '<iframe id="doc" src="https://docs.google.com/forms/d/e/1FAIpQLSfcD4QbQJF8N9RWkSFfYkmQL09JHAxR60-YnqgZ0VVoGLktgw/viewform?embedded=true" width="99%" height="820" frameborder="0" marginheight="0" marginwidth="0">Cargando...</iframe>',
+    '<iframe id="doc" src="https://docs.google.com/forms/d/e/1FAIpQLSduacefR3VnaNRTTET9Adu_wZ9Khg-ydAzLGzzLuvP0aa6iVA/viewform?embedded=true" width="99%" height="820" frameborder="0" marginheight="0" marginwidth="0">Cargando...</iframe>',
+    '<iframe id="doc" src="https://docs.google.com/forms/d/e/1FAIpQLSeaU0UOnJfK79ZfyhUCUR_xCxxGzLKHXX_miuevDqr5_WMrvw/viewform?embedded=true" width="99%" height="820" frameborder="0" marginheight="0" marginwidth="0">Cargando...</iframe>',
+    '<iframe id="doc" src="https://docs.google.com/forms/d/e/1FAIpQLSeGIdzumInqd_IRKRxu4ZO030rR8xeMPKHex6KU3T8xhJX0GA/viewform?embedded=true" width="99%" height="820" frameborder="0" marginheight="0" marginwidth="0">Cargando...</iframe>'
+]
+*/
+//UNAM
+var areasArray=[
+    '<iframe id="doc" src="https://docs.google.com/forms/d/e/1FAIpQLSfu6VPx8GlRE5KV39Fv67FnTIUOnrKjFU1rPme4-qNlqt7qvA/viewform?embedded=true" width="99%" height="820" frameborder="0" marginheight="0" marginwidth="0">Cargando…</iframe>',
+    '<iframe id="doc" src="https://docs.google.com/forms/d/e/1FAIpQLSe-7qrw687ZxFpsQtS-Q4icpGpDvUfAvfgScjQuv9kUvy2awQ/viewform?embedded=true" width="99%" height="820" frameborder="0" marginheight="0" marginwidth="0">Cargando…</iframe>',
+    '<iframe id="doc" src="https://docs.google.com/forms/d/e/1FAIpQLSe-i5Ii2YcLp7zrUqzAFDD6xb8n8Pup3_qJFnF_LC44oAqpFQ/viewform?embedded=true" width="99%" height="820" frameborder="0" marginheight="0" marginwidth="0">Cargando…</iframe>',
+    '<iframe id="doc" src="https://docs.google.com/forms/d/e/1FAIpQLSfa49RgA4SJThYJKj8dhMeIJhxyujMoQHi7YXJ2skZCL0InzQ/viewform?embedded=true" width="99%" height="820" frameborder="0" marginheight="0" marginwidth="0">Cargando…</iframe>'
+]
+
+function print(e){
+    document.getElementsByTagName('h1')[0].style.display='none'
+    document.getElementById("navSection").style.display='none'
+    var tipo =e.getAttribute('data-tipo')
+    var area =e.getAttribute('data-area')
+    
+    document.getElementById("content").style.display='block'
+    //var exam= `pdf/Examen UAM ${tipo}.pdf`
+    var exam= `pdf/guiaUNAM A${tipo}.pdf`
+    PDFObject.embed(exam, "#example1");
+    
+    var formulario = document.getElementById("answers")
+
+    formulario.innerHTML=areasArray[area-1]
+
+    document.getElementById("content").style.height=`${maxAlt}px`
+    document.getElementById('doc').setAttribute("height",maxAlt);
+
+    contador();
+
+}
+
+function contador(){
+    var later=new Date();
+    later.setHours(later.getHours()+3);
+
+    console.log(later)
+
+    $('#clock').countdown(later, function(event) {
+        $(this).html(event.strftime('%H:%M:%S'));
+    });
+}
