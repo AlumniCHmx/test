ee3a5c60f40ba9a21443b796de8d6721aac9b841
JesusMoralesF <emorales41@gmail.com>
2020-07-04 20:05:40 -0500

    PDF added

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-var altura=window.innerHeight;
-var maxAlt=altura-25;
-
 var cajas=document.querySelectorAll('.blurred-box');
 cajas.forEach(function(element) {
     element.setAttribute('onclick','print(this)')
@@ -14,12 +11,21 @@ var areasArray=[
     '<iframe id="doc" src="https://docs.google.com/forms/d/e/1FAIpQLSeGIdzumInqd_IRKRxu4ZO030rR8xeMPKHex6KU3T8xhJX0GA/viewform?embedded=true" width="99%" height="820" frameborder="0" marginheight="0" marginwidth="0">Cargando...</iframe>'
 ]
 */
+
+/*
 //UNAM
 var areasArray=[
     '<iframe id="doc" src="https://docs.google.com/forms/d/e/1FAIpQLSfu6VPx8GlRE5KV39Fv67FnTIUOnrKjFU1rPme4-qNlqt7qvA/viewform?embedded=true" width="99%" height="820" frameborder="0" marginheight="0" marginwidth="0">Cargando…</iframe>',
     '<iframe id="doc" src="https://docs.google.com/forms/d/e/1FAIpQLSe-7qrw687ZxFpsQtS-Q4icpGpDvUfAvfgScjQuv9kUvy2awQ/viewform?embedded=true" width="99%" height="820" frameborder="0" marginheight="0" marginwidth="0">Cargando…</iframe>',
     '<iframe id="doc" src="https://docs.google.com/forms/d/e/1FAIpQLSe-i5Ii2YcLp7zrUqzAFDD6xb8n8Pup3_qJFnF_LC44oAqpFQ/viewform?embedded=true" width="99%" height="820" frameborder="0" marginheight="0" marginwidth="0">Cargando…</iframe>',
     '<iframe id="doc" src="https://docs.google.com/forms/d/e/1FAIpQLSfa49RgA4SJThYJKj8dhMeIJhxyujMoQHi7YXJ2skZCL0InzQ/viewform?embedded=true" width="99%" height="820" frameborder="0" marginheight="0" marginwidth="0">Cargando…</iframe>'
+]*/
+
+//IPN
+var areasArray = [
+ `<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdq6QfoAEtsWx-yb5N6IpfwSM2dosh79LR76b-1Z8JIgTSjfw/viewform?embedded=true" width="99%" height="${window.innerHeight-25}" frameborder="0" marginheight="0" marginwidth="0">Cargando…</iframe>`,
+ `<iframe src="https://docs.google.com/forms/d/e/1FAIpQLScQO_hZtnJkrCUIKoihhDjpH61GL1LEn-Yl7HXnlukEOIf6hg/viewform?embedded=true" width="99%" height="${window.innerHeight-25}" frameborder="0" marginheight="0" marginwidth="0">Cargando…</iframe>`,
+ `<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfxvx4JNdBT1dOTdik6_0vG1RJnQAkNjpB7o7KgvLWanj2uhA/viewform?embedded=true" width="99%" height="${window.innerHeight-25}" frameborder="0" marginheight="0" marginwidth="0">Cargando…</iframe>`   
 ]
 
 function print(e){
@@ -29,16 +35,17 @@ function print(e){
     var area =e.getAttribute('data-area')
     
     document.getElementById("content").style.display='block'
-    //var exam= `pdf/Examen UAM ${tipo}.pdf`
-    var exam= `pdf/guiaUNAM A${tipo}.pdf`
+    //var exam = `pdf/Examen UAM ${tipo}.pdf`
+    var exam = `pdf/IPN-${tipo}.pdf`
     PDFObject.embed(exam, "#example1");
     
     var formulario = document.getElementById("answers")
-
     formulario.innerHTML=areasArray[area-1]
 
-    document.getElementById("content").style.height=`${maxAlt}px`
-    document.getElementById('doc').setAttribute("height",maxAlt);
+    //setting height of PDF
+    document.getElementById("content").style.height=`${window.innerHeight-25}px`
+    // setting height of FORM
+    //document.getElementById('doc').setAttribute("height",window.innerHeight);
 
     contador();
 
