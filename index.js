98efe48dd02da5f02a4ad2eb90ef0ee39f8be13d
Nativo_Jesus_Morales <chucho@Nativo-Jesus-Morales.local>
2021-02-01 15:51:00 -0600

    January 30 update

--- index.js
+++ index.js
@@ -5,8 +5,7 @@ cajas.forEach(function (element) {
 
 // IPN
 var areasArray = [
-  `<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdmaAqEj5U9hQ_EBXLMiOMqj5x8dEUMja15LjtgHC7uKGWboQ/viewform?embedded=true" width="99%" height="${window.innerHeight - 25}" frameborder="0" marginheight="0" marginwidth="0">Cargando…</iframe>`
-  // `<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeDnff11bSGXlyciaznrmGKM0eU_L1uplk4DWVlsEfPumvksQ/viewform?embedded=true" width="99%" height="${window.innerHeight-25}" frameborder="0" marginheight="0" marginwidth="0">Cargando…</iframe>`
+  `<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSftRzDeTwEme9RUlgiFwi1cuVUgMFcRKsOOr3YAKiH-SfEKTA/viewform?embedded=true" width="99%" height="${window.innerHeight-25}" frameborder="0" marginheight="0" marginwidth="0">Cargando…</iframe>`
 ]
 
 function print (e) {
@@ -17,7 +16,7 @@ function print (e) {
 
   document.getElementById('content').style.display = 'block'
   // var exam = `pdf/Examen UAM ${tipo}.pdf`
-  let exam = `pdf/UNAM-${tipo}.pdf`
+  let exam = `pdf/ENERO2021-${tipo}.pdf`
   PDFObject.embed(exam, '#example1')
 
   var formulario = document.getElementById('answers')
